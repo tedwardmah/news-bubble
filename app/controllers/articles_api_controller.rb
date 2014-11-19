ee237e6bf37d7dc700f96445578839621b8069e1
Evan <evturn>
2014-11-19 16:31:40 -0500

    filling article table with usa today data

--- app/controllers/articles_api_controller.rb
+++ app/controllers/articles_api_controller.rb
@@ -4,17 +4,17 @@ class ArticlesAPIController < ApplicationController
   def index
 
     response = HTTParty.get('http://api.usatoday.com/open/articles/topnews/sports?api_key=mnsmzv8pam9p2p2sswj4efs8')
-
     arr = response["rss"]["channel"]["item"]
     
-    titles = arr.map do |a|
-      a["title"]
-      end
-    links = arr.map do |a|
-      a["link"]
+    arr.each do |a|
+      hash = {}
+      hash[:headline] = a['title']
+      hash[:url] = a['link']
+      hash[:date] = a['pubDate']
+      Article.create!(hash)
     end
-    
-    
+  
+  @article = Article.all
 
   end
 
