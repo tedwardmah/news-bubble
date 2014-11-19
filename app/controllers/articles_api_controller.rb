18988a324d551819886aee81d5424e344384b186
tedwardmah <ted.mahoney22@gmail.com>
2014-11-19 18:00:46 -0500

    adds starter code for NYT api request

--- app/controllers/articles_api_controller.rb
+++ app/controllers/articles_api_controller.rb
@@ -5,7 +5,7 @@ class ArticlesAPIController < ApplicationController
 
     response = HTTParty.get('http://api.usatoday.com/open/articles/topnews/sports?api_key=mnsmzv8pam9p2p2sswj4efs8')
     arr = response["rss"]["channel"]["item"]
-    
+
     arr.each do |a|
       hash = {}
       hash[:headline] = a['title']
@@ -13,8 +13,22 @@ class ArticlesAPIController < ApplicationController
       hash[:date] = a['pubDate']
       Article.create!(hash)
     end
-  
+
     @article = Article.all
+
+  end
+
+  def fetch_nyt_articles(section = 'sports') #this defaults to sports for the moment but should eventually be dynamic
+    section = "sports"
+    nyt_api_key = '53d66b4923e199e21d3304cfc06527ea:3:57497200'
+    nyt_url = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/#{section}/1.json?api-key=#{nyt_api_key}"
+    api_response = HTTParty.get(nyt_url)
+    articles = api_response['results']
+    articles.each do |article_data|
+      hash = {}
+      hash[:url] = article_data['url']
+    end
+
 
   end
 
