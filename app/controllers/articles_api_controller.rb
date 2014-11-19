0e31bfeb1978891f7156aa3adecd0cd33ee27480
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-11-19 16:52:44 -0500

    api_controller

--- app/controllers/articles_api_controller.rb
+++ app/controllers/articles_api_controller.rb
@@ -2,13 +2,7 @@ class ArticlesAPIController < ApplicationController
 
 
   def index
-
-    response = HTTParty.get('http://api.usatoday.com/open/articles/topnews/sports?api_key=mnsmzv8pam9p2p2sswj4efs8')
-
-    articles_array = response["rss"]["channel"]["item"]
-    
- 
-  end
-
-
+	
+		end
+	end
 end
