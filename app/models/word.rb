ed94a478af059ec20abef35a651fb8173abe1e1a
tedwardmah <ted.mahoney22@gmail.com>
2014-11-20 11:06:32 -0500

    word model can 'parse' headlines stupidly

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,18 @@
 class Word < ActiveRecord::Base
   has_many :mentions
   has_many :articles, :through => :mentions
+
+
+  def self.parse_articles_stupidly
+    headlines = Article.all.map do |article|
+      article.headline
+    end
+    headlines.each do |headline|
+      words = headline.split(' ')
+      words.each do |word|
+        Word.create({keyword: word.downcase})
+      end
+    end
+  end
+
 end
