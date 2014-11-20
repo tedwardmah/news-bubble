2b14eec8c45059da367f17de857eec96d432d368
Evan <evturn>
2014-11-20 17:47:27 -0500

    bubble table needs inspecting

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,7 +2,15 @@ class WordsController < ApplicationController
 
   def index
     @word = Word.all
+    # file = File.read('app/assets/resources/flare.json')
+    # thing = JSON.parse(file)
+    # puts thing
   end
+
+  def show
+
+  end
+
 
 
 end
