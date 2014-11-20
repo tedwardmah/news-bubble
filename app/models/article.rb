28867246839f3b3a3855e550cfa4c0cc5ddb5139
tedwardmah <ted.mahoney22@gmail.com>
2014-11-20 12:00:16 -0500

    Articles can now parse their own headlines to create words and mentions
    Parsing creates a hash that stores words and the number of times those words appear in
    the headline as a key:value pair. Words will only be created in the database if they
    don't already exist.  Mentions are linked to words and articles...parsing is still dumb
    and includes punctuation, etc...

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,34 @@ class Article < ActiveRecord::Base
   belongs_to :api
   has_many :mentions
   has_many :words, :through => :mentions
+
+  def parse_article
+    headline = self.headline
+    words = headline.split(' ')
+    words_count_pairs = Hash.new(0)
+    words.each do |word|
+      words_count_pairs[word.downcase] += 1
+    end
+    create_mentions(words_count_pairs)
+  end
+
+  def create_mentions(words_count_pairs)
+    words_count_pairs.each do |word, count|
+      if existing_word = Word.find_by(keyword: word)
+        Mention.create({
+          word_id: existing_word.id,
+          article_id: self.id,
+          word_count: count,
+          })
+      else
+        new_word = Word.create({keyword: word})
+        Mention.create({
+          word_id: new_word.id,
+          article_id: self.id,
+          word_count: count,
+          })
+      end
+    end
+  end
+
 end
