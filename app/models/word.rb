ef5a1f039dc761ee7cd9d4e42bf312d4c1c1ae70
tedwardmah <ted.mahoney22@gmail.com>
2014-11-20 16:57:12 -0500

    removed parse_stupidly from word model since we don't use it anymore

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,19 +2,6 @@ class Word < ActiveRecord::Base
   has_many :mentions
   has_many :articles, :through => :mentions
 
-
-  def self.parse_articles_stupidly
-    headlines = Article.all.map do |article|
-      article.headline
-    end
-    headlines.each do |headline|
-      words = headline.split(' ')
-      words.each do |word|
-        Word.create({keyword: word.downcase})
-      end
-    end
-  end
-
   def self.sort_by_total_count(num_results)
     words = self.all
     sorted_words = words.sort_by do |word|
