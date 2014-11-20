c0f76506bf6a5393c3668e29a4e1999f3c70c435
tedwardmah <ted.mahoney22@gmail.com>
2014-11-19 19:26:56 -0500

    nyt raketask and api call implemented

--- app/controllers/articles_api_controller.rb
+++ app/controllers/articles_api_controller.rb
@@ -28,7 +28,6 @@ class ArticlesAPIController < ApplicationController
   end
 
   def fetch_nyt_articles(section = 'sports') #this defaults to sports for the moment but should eventually be dynamic
-    section = "sports"
     nyt_api_key = '53d66b4923e199e21d3304cfc06527ea:3:57497200'
     nyt_url = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/#{section}/1.json?api-key=#{nyt_api_key}"
     api_response = HTTParty.get(nyt_url)
