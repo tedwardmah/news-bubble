c284ff9169eadc7686e7a2896ea8856716038b92
tedwardmah <ted.mahoney22@gmail.com>
2014-11-21 09:53:47 -0500

    excluded words are excluded

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,11 +3,22 @@ class Word < ActiveRecord::Base
   has_many :articles, :through => :mentions
 
   def self.sort_by_total_count(num_results)
-    words = self.all
+    #words = self.all
+    # new shit
+    words = get_good_words
+
+    # end of new shit
     sorted_words = words.sort_by do |word|
-      word.total_count != arr
+      word.total_count
     end
-    sorted_words.reverse[0..(num_results-1)] 
+    sorted_words.reverse[0..(num_results-1)]
+  end
+
+  def self.get_good_words
+    all_words = self.all.map {|word| word}
+    excluded_words_raw = File.readlines('./app/assets/files/excluded_words.txt')
+    excluded_words = excluded_words_raw.map {|word| word.chomp }
+    all_words.reject! {|word| excluded_words.include?(word.keyword)}
   end
 
   def total_count
