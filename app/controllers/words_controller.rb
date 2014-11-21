b6f20df4e1b697ce50619851ed86aa7f5085c423
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-11-21 14:08:16 -0500

    removed <<<<<<< HEAD from words_controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,16 +6,11 @@ class WordsController < ApplicationController
     # file = File.read('app/assets/resources/flare.json')
     # thing = JSON.parse(file)
     # puts thing
-<<<<<<< HEAD
   end
 
   def show
   	word = Word.find(params[:id])
   	@articles = word.articles
-=======
-
-    
->>>>>>> 8bf27849cc46cecdfdb74263c0b278f995f34691
   end
 
 end
