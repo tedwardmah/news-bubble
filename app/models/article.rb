6096e4fc84517c870b4b20e144f5794bde679089
tedwardmah <ted.mahoney22@gmail.com>
2014-11-21 13:15:55 -0500

    regex word scanning is working ok enough for now

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,18 +7,20 @@ class Article < ActiveRecord::Base
   def parse_article
     words_count_pairs = Hash.new(0)
     #headline parse
-    headline = self.headline.gsub(/'s/, '')
-    headline.gsub!(/\\n|/, ' ')
-    headline.gsub!(/\[.+\]|&#8217;|[.,:;'?!]|\d+-\d+|\d+ - \d+|\(.+\)/, '')
-    headline_words = headline.split(' ')
+    # headline = self.headline.gsub!(/'s/, '')
+    # headline.gsub!(/\\n/, ' ')
+    # headline.gsub!(/\[.+\]|&#8220;|&#8217;|[.,:;'?!]|\d+-\d+|\d+ - \d+| - /, '')
+    # headline_words = headline.split(' ')
+    headline_words = self.headline.scan(/\b[a-zA-z]+-?[a-zA-Z]+\b/)
     headline_words.each do |word|
       words_count_pairs[word.downcase] += 1
     end
     #lead parse
-    lead = self.lead.gsub(/'s/, '')
-    lead.gsub!(/\\n|/, ' ')
-    lead.gsub!(/\[.+\]|&#8217;|[.,:;'?!]|\d+-\d+|\d+ - \d+|\(.+\)/, '')
-    lead_words = lead.split(' ')
+    # lead = self.lead.gsub(/'s/, '')
+    # lead.gsub!(/\\n/, ' ')
+    # lead.gsub!(/\[.+\]|&#8220;|&#8217;|[.,:;'?!]|\d+-\d+|\d+ - \d+| - /, '')
+    # lead_words = lead.split(' ')
+    lead_words = self.lead.scan(/\b[a-zA-z]+-?[a-zA-Z]+\b/)
     lead_words.each do |word|
       words_count_pairs[word.downcase] += 1
     end
