3dd4f6ed07a35f7e54c9de06e63a712a5bdf72f8
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-11-21 17:04:53 -0500

    added sort_by_total_count to word_controller.rb

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,7 +2,7 @@ class WordsController < ApplicationController
 
   def index
 
-    @words = Word.all
+    @words = Word.ort_by_total_count(40)
     # file = File.read('app/assets/javascripts/flare.json')
     # file = File.read('app/assets/resources/flare.json')
     # thing = JSON.parse(file)
