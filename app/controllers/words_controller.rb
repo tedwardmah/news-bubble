0d25ba98bd96dc5dd7f0e13433c1f2ebc944da5f
Poopsik <vadimvny@Vadims-MacBook-Pro.local>
2014-11-21 16:52:31 -0500

    fixed error in words_controller.rb

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,17 +2,15 @@ class WordsController < ApplicationController
 
   def index
 
-    @word = Word.all
-    file = File.read('app/assets/javascripts/flare.json')
+    @words = Word.all
+    # file = File.read('app/assets/javascripts/flare.json')
     # file = File.read('app/assets/resources/flare.json')
     # thing = JSON.parse(file)
     # puts thing
-    respond_to do |format|
-      format.html
-      format.json { render :json => { words: @words }}
-  end
-end
-
+  #   respond_to do |format|
+  #     format.html
+  #     format.json { render :json => { words: @words }}
+  	end
 
   def show
   	word = Word.find(params[:id])
