b35d99f1b87e35c986aeea74b2d858e5c5803b3c
Evan <evturn>
2014-11-21 16:41:37 -0500

    hi

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,11 +2,14 @@ class WordsController < ApplicationController
 
   def index
     @word = Word.all
+    file = File.read('app/assets/javascripts/flare.json')
     # file = File.read('app/assets/resources/flare.json')
     # thing = JSON.parse(file)
     # puts thing
-
-    
+    respond_to do |format|
+      format.html
+      format.json { render :json => { words: @words }}
+    end
   end
 
 end
