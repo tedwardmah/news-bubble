9917ccc58420e12bd99a0790a7f755ca7ca9e2b6
tedwardmah <ted.mahoney22@gmail.com>
2014-11-22 17:34:36 -0500

    words show page renders articles html using javascript

--- app/assets/javascripts/articles/collections/ArticleList.js
+++ app/assets/javascripts/articles/collections/ArticleList.js
@@ -11,5 +11,19 @@ ArticleList.prototype.add = function add(model){
 }
 
 ArticleList.prototype.fetch = function fetch(){
-  
+  var word_id = $('.ghostface').data('word-id');
+  var url = ('/words/' + word_id);
+  var that = this;
+  $.ajax({
+    url: url,
+    method: 'GET',
+    dataType: 'json',
+    success: function(data){
+      that.models = [];
+      $(data.articles).each(function(index, articleOptions) {
+        var article = new that.model(articleOptions);
+        that.add(article);
+      })
+    }
+  })
 }
