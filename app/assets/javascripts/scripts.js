f1a2b298693a5a6396094537403aa9f8633fbe04
Evan Turner <evturn@gmail.com>
2014-11-23 15:58:16 -0500

    commited out scripts

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,55 +1,33 @@
-function slideLeft(){
-	$('.articles').animate({width:'slideToggle'}, 350);
-}
-
-function hideLead() {
-	$('.lead').hide();	
-}
 $( document ).ready(function(){
 
-	$(".article").hover(function(){
-	    $(this).animate({ width: "800px", height: "70px" });
-			}, function() {
-	    	$(this).animate({ width: "820px", height: "80px" });
-		});
-
-	$(".lead").hide();
-
-	$(".articles").on("click", function() {
-		$(this).next().slideToggle();
-		$(this)slide("left");
-	});
-
-	$(".circles").hover(function(){
-		$(this).animate({ width: "250px", height: "250px"})
-		}, function() {
-	    	$(this).animate({ width: "200px", height: "200px" });
-	})
-
-	$(".circles").hide();
-
-	$(".start-btn").on("click", function() {
-		$(".circles").next().slideToggle();
-		$(".start").hide();
-		return false;
-	});
-
-	// var el = $(".circles")
-	// el.on("click", function(event){
-	// 	event.preventDefault();
-	// 	$.ajax('/words/:id' {
-	// 		type: 'GET',
-	// 		data: word_id,
-	// 		dataType: 'json',
-	// 		success: function() {
-	// 			$('.articles').html
 
 
-	// 		}
-
+	// $(".article").hover(function(){
+	//     $(this).animate({ width: "800px", height: "70px" });
+	// 		}, function() {
+	//     	$(this).animate({ width: "820px", height: "80px" });
 	// 	});
+
+	// $(".lead").hide();
+
+	// $(".articles").on("click", function() {
+	// 	$(this).next().slideToggle();
+	// 	$(this)slide("left");
 	// });
 
+	// $(".circles").hover(function(){
+	// 	$(this).animate({ width: "250px", height: "250px"})
+	// 	}, function() {
+	//     	$(this).animate({ width: "200px", height: "200px" });
+	// })
+
+	// $(".circles").hide();
+
+	// $(".start-btn").on("click", function() {
+	// 	$(".circles").next().slideToggle();
+	// 	$(".start").hide();
+	// 	return false;
+	// });
 
 
 });
